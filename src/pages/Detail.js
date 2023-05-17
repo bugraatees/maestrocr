3e53942a88e495bdbc28d7418989b89bbb5ae71e
bugraatees <bugraatees@hotmail.com>
2023-05-17 18:12:51 +0300

    v1

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState , useEffect} from 'react'
 import {SafeAreaView, Text, View } from 'react-native'
+import axios from 'axios'
 
-const Detail = () => {
+function Detail(id){
+
+  async function fetchData(){
+    const response = await axios.GET('https://www.lenasoftware.com/api/v1/en/maestro/1')
+    console.log(id)
+  }
+  useEffect(()=> {
+    fetchData();  
+  }, [])
+
   return (
-    <View>
-      <Text>Forum</Text>
-    </View>
+    <SafeAreaView style={{flex:1,}}>
+      <View style={{margin: 10,}}>
+       <Text style={{fontSize:18, fontWeight:'600'}}>{id.content}</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,13 +2,21 @@ import React, {useState, useEffect} from 'react'
 import { SafeAreaView, ActivityIndicator, Text, View, FlatList, Image, Dimensions, TouchableOpacity } from 'react-native'
 import axios from 'axios';
 
-function HomePage(){
+function HomePage(props){
     const[loading, setLoading] = useState(false)
     const[liste, setListe ] = useState([]);
+    const[refreshing, setRefreshing] = React.useState(false);
 
+    const onRefresh = React.useCallback(() => {
+      setRefreshing(true);
+      setTimeout(() => {
+        setRefreshing(false);
+      }, 2000);
+    }, []);
+  
     async function fetchData(){
         setLoading(true)
-        const response = await axios.get('https://jsonplaceholder.typicode.com/comments?postId=1');
+        const response = await axios.get('https://www.lenasoftware.com/api/v1/en/maestro/1');
         setLoading(false)
         setListe(response.data)
     }
@@ -25,17 +33,32 @@ function HomePage(){
         {loading ? ( 
         <ActivityIndicator size="large" />) : (
         <FlatList 
-        keyExtractor={item => item.id}
         data={liste}
         renderItem={({item}) =>
-        <View style={{borderWidth:1, borderColor:'#bdbdbd'}}>
+        <View style={{borderWidth:1, borderColor:'#bdbdbd', flex:1}}>
+           refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
           <TouchableOpacity>
-            <Text>{item.name}</Text>
+            <Text style={{color:'red'}}>{item.title}</Text>
           </TouchableOpacity>
         </View>
         }
         />
         )}
+        <View style={{ borderWidth:1, borderColor:'#bdbdbd', borderRadius:20}}>
+          <TouchableOpacity onPress={() => props.navigation.navigate('Detail', {id})}>
+           <Image style={{width:Dimensions.get('window').width/1, height:Dimensions.get('window').height /3}} source={require('../images/1.jpg')} />
+           <View style={{margin:5}}>
+            <Text style={{fontSize:15, fontWeight:'bold'}}>Automating the Change Request Process: A must-have for IT Projects</Text>
+            <Text>Automating the Change Request Process: A must-have for IT Projects</Text>
+              <View style={{flexDirection:'row'}}>
+               <Text style={{color:'red', fontSize:18}}>totalReadingTime : </Text>
+               <Text style={{ fontSize:18}}>-16</Text>
+              </View>
+           </View> 
+          </TouchableOpacity>
+        </View>
      </View>
     </SafeAreaView>
   )
